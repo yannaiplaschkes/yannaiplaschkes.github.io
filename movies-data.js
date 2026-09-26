31694e61ad423b62fcdf8d0be0f259d9c4705e54
yannaiplaschkes <yannaiplaschkes@gmail.com>
2026-09-26 11:48:18 +0200

    Update movies-data.js
    fixed

--- movies-data.js
+++ movies-data.js
@@ -18,7 +18,7 @@ const myMovies = [
             "chairbehindts1.jpg",
             "chairbehindts2.jpg"
         ],
-        description 2: "longer story coming soon..."
+        closingNote: "longer story coming soon...",
         youtubeUrl: "https://www.youtube.com/watch?v=oc_jo4OjmtA",
         trailerUrl: "https://www.youtube.com/watch?v=4YybBfvTX3M",
         letterboxdUrl: "https://letterboxd.com/film/the-life-of-a-chair/",
@@ -33,6 +33,7 @@ const myMovies = [
         status: "upcoming",
         info: "Currently in development...",
         description: "",
+        closingNote: "",
         posterUrl: "",
         galleryPhotos: [],
         behindTheScenes: [],
